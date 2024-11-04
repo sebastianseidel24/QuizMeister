d0da091c11f6d25fba51be6cfc0910996f6175f5
18sebi60 <sebastianseidel24@gmail.com>
2024-11-04 11:09:32 +0100

    Erste HTML Seiten erstellt

--- quizzy.py
+++ quizzy.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello, World!"
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/quizoverview")
+def quizoverview():
+    return render_template("quizoverview.html")
+
+@app.route("/createquiz")
+def createquiz():
+    return render_template("createquiz.html")
+
+@app.route("/quiz")
+def quiz():
+    return render_template("quiz.html")
+
+@app.route("/results")
+def results():
+    return render_template("results.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
