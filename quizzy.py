082bc8aedda26a6f3f6f26b14c4c3907ce848d16
18sebi60 <sebastianseidel24@gmail.com>
2024-12-04 11:16:35 +0100

    hostquiz and playquiz routes established

--- quizzy.py
+++ quizzy.py
@@ -226,9 +226,23 @@ sessions = [{"session_id": "1234", "players": {}}]
 #     return render_template("joinquiz.html")
 
 
-@app.route('/hostquiz')
-def hostquiz():
-    return render_template("hostquiz.html")
+@app.route('/hostquiz/<int:quiz_id>')
+def hostquiz(quiz_id):
+    with sqlite3.connect('quizzy.db') as con:
+        cur = con.cursor()
+
+        # Quiz-Daten laden
+        cur.execute("SELECT rowid, * FROM quizzes WHERE rowid = (?)", (quiz_id,))
+        quiz = cur.fetchone()
+
+        # Fragen des Quizzes laden
+        cur.execute("SELECT * FROM questions WHERE quiz_id = (?)", (quiz_id,))
+        questions = cur.fetchall()
+
+    if quiz == None:
+        return "Quiz nicht gefunden.", 404
+    else:
+        return render_template("hostquiz.html", quiz=quiz, questions=questions)
 
 
 
